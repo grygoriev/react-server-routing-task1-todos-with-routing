98621fc657f3e51bf79746d3ef5de0a2934fc3bc
grygoriev <vovagrigoriev@gmail.com>
2024-12-14 09:01:31 +0200

    change to JSX again

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-// Данный импорт - декларативное описание зависимости
-// (указывает, какой ресурс нужен, но не говорит, "как" именно его обрабатывать).
 import ReactLogo from './assets/react.svg?react';
-import { createElement } from 'react';
 
 export const App = () => {
-	// Начинается декларативный стиль: мы описываем желаемую структуру,
-	// а не говорим пошагово, как её создавать или изменять.
-	return createElement(
-		'div',
-		{ className: 'container' },
-		createElement('h1', null, 'Hello World!'),
-		createElement(ReactLogo), // Декларативно вставляем компонент
-		createElement('hr'),
-		// Здесь вызывается метод new Date().getFullYear().
-		// Сам вызов — императивен, но результат используется в декларативном описании интерфейса.
-		createElement('span', { className: 'span-year' }, new Date().getFullYear()),
+	return (
+		<div className="container">
+			<h1>Hello World!</h1>
+			<ReactLogo />
+			<hr />
+			<span className="span-year">{new Date().getFullYear()}</span>
+		</div>
 	);
-	// На этом месте декларативный стиль для UI завершается.
 };
